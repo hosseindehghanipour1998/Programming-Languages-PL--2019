13cb80bd7dd9c58bde2c1246951d1e3652f8acfe
Hossein <hossein.dehghanipour1998@gmail.com>
2019-11-29 02:39:29 +0330

    Added HW 1 -> Question 3 -> part I & II

--- HW 1/Question 3.scm
+++ HW 1/Question 3.scm
@@ -1,5 +1,13 @@
 (display 'Welcome!)
 (newline)
+(display 'Hossein_Dehghanipour_9532250)
+(newline)
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+; QUESTION 3 - PART I
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+
 (define (calculateRow x )
 	(cond 
 		( (null? x ) 0 )
@@ -10,14 +18,58 @@
 )
 
 (define (rowSum x)
-		(cond 
-			( (null? x ) () )
-			(else 
-				(cons (calculateRow (car x)) (rowSum(cdr x )) )
-			)
+	(cond 
+		( (null? x ) () )
+		(else 
+			(cons (calculateRow (car x)) (rowSum(cdr x )) )
 		)
+	)
 )
 
-	
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+; QUESTION 3 - PART II
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
-(display (rowSum '( (0 1 2 3 4) (5 6 7 8 9) (0 1 2 3 4) (5 6 7 8 9) ) ) )
+(define (calculateColumn x )
+
+	(cond 
+		( (null? x ) 0 )
+		(else
+			( + ( caar x) ( calculateColumn ( cdr x ) ) )
+		)
+	)
+)	
+
+(define (colSum x)
+	
+	(cond 
+		( (null?  (car x )) () )
+		(else   
+			( cons ( calculateColumn x ) ( colSum (map cdr x ) )  )
+			
+		)
+	)
+)
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+; QUESTION 3 - MAIN
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(define (main)
+	;;;;;;;;;;;;;;; Call Part I ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+	(display 'Part_I_Answer:)
+	(newline)
+	(display (rowSum '( (0 1 2 3 4) (5 6 7 8 9) (0 1 2 3 4) (5 6 7 8 9) ) ) )
+	;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+	;;;;;;;;;;;;;;; Call Part II ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+	(newline)
+	(display 'Part_II_Answer:)
+	(newline)
+	(display (colSum '((0 1 2 3 4) (5 6 7 8 9) (0 1 2 3 4) (5 6 7 8 9))) )
+
+	;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+	0 ; return 0 ( as we do in C )
+)
+
+(main)
+
