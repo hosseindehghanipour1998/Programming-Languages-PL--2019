4071fde89ddb20c39afa4d5a194cc3f23a273b86
Hossein <hossein.dehghanipour1998@gmail.com>
2019-12-20 17:34:40 +0330

    Starting fresh

--- Kotlin/Sample Codes/src/Main.kt
+++ Kotlin/Sample Codes/src/Main.kt
@@ -1,4 +1,8 @@
 class Main {
 
+    fun main(args: Array<String>) {
 
+        val strings = hashSetOf("a", "b", "c", "c" , "a" , "f" , "hossein")
+        println("My Set Values are"+strings)
+    }
 }
