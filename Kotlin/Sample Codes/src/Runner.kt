02e0d90e04eea18a3280b7b5c5ce0ef31260f6bd
Hossein <hossein.dehghanipour1998@gmail.com>
2019-12-20 17:54:24 +0330

    Starting Ranges

--- Kotlin/Sample Codes/src/Runner.kt
+++ Kotlin/Sample Codes/src/Runner.kt
@@ -1,6 +1,34 @@
 
 fun main(args: Array<String>) {
 
-    //Arrays
+    // =============== ARRAYS =================
+    var myArray = arrayOf(1 , 'a'..'z' , 1.23 , 2.25 , "Lion") ;
+    val animal = "Lion"
+    //arrays can contain objects with different types like C#
+
+    println("Array Length : ${myArray.size}")
+    println("Contain 'z' ? ${myArray.contains(animal)}")
+    // get a subset of the following array
+    var subset = myArray.copyOfRange(3,5);
+    for ( item in subset){
+        print("$item  ")
+    }
+    //get first element of the array
+    println("\nFirst Element : ${myArray.first()}")
+    //get index of an object
+    println("Index of '$animal' = ${myArray.indexOf(animal)}")
+
+    // Lambada in Arrays
+    var sqrArray = Array( 5 , {x-> x*x})
+    for (item in sqrArray){
+        print("$item  ")
+    }
+
+    // Type specific Arrays
+    var arr2 : Array<Int> = arrayOf(1,2,3,4,5,6);
+
+
+    //===================== RANGES ========================
 
 }
+
